788c403f8d37daab5cf1bbcea4f4d4d5df1cebc2
Çağlar Dursun <dursuncaglar@gmail.com>
2020-09-28 02:34:33 +0300

    .

--- basics/src/primitive_types.rs
+++ basics/src/primitive_types.rs
@@ -1,12 +1,13 @@
-pub fn tuple_types(){
-
+pub fn tuple_types()
+{
+    println!("\n");
     let t = (1,'a',false);
     let f = (1,'a',(1,'a',false));
     println!("{} {} {}",t.0,t.1,t.2);
-    //# means formatting indentation here 
+    //# means formatting & indentation here 
     println!("{:#?}",f);
 
-
+    println!("\n");
 }
 
 pub fn array_types()
@@ -18,15 +19,25 @@ pub fn array_types()
     println!("Size of value : {}",std::mem::size_of_val(&xs));
     let slices = &xs[1..4];
     println!("{:?}",slices);
+    println!("\n");
 }
 
-pub fn string_types(){
-
+pub fn string_types()
+{    
     let s = "Some &str";
     println!("{}",s);
+
+
     let s : String = String::from("Çağlar Dursun");
     println!("{}",s);
-    
+    //Another string object
+    let ss = "Some &str converted into String".to_string();
+    println!("{}",ss);
+
+    //like in python x[:5] 
+    let slice_of = &ss[0..5];
+    println!("Slice of string : {}",slice_of);
+
     println!("Name contains Çağlar : {}",s.contains("Çağlar"));
-    
+    println!("\n");
 }
