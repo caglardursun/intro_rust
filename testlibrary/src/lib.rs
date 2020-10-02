ce31ff15cb28224ca5afbe77092048f563d9cfad
Çağlar Dursun <dursuncaglar@gmail.com>
2020-10-03 01:14:43 +0300

    .

--- testlibrary/src/lib.rs
+++ testlibrary/src/lib.rs
@@ -1,18 +1,37 @@
-
+/// Fibonacci number calculator for the given index
+/// ```
+///use testlibrary::*;
+/// 
+///    fn main(){
+///         let number = fibonacci(6).unwrap();
+///         //or
+///         let result = match fibonacci(-1)
+///         {
+///             Ok(value)=> value,
+///             Err(e)=> 0,
+///         };
+///     
+///    }
+/// 
+/// 
+/// ```
+/// 
 pub fn fibonacci(i:i32) -> Result<i32,String>
 {
+    if i <= 0 
+    {
+        return Err("Cannot be negative".to_string());
+    };
 
-    // match i {
-        
-    // }
+    let result =  match i {
+        1 => Ok(1),
+        2 => Ok(1),
+        _ =>  Ok(fibonacci(i-1).unwrap() + fibonacci(i-2).unwrap()),     
+    };
 
-    // if i == 1 || i == 2
-    // {
-    //     Ok(1)
-    // }
-    // else{
-    //     fibonacci(i-1) + fibonacci(i-2)
-    // }
+    
+    result
+    
 }
 
 
@@ -21,6 +40,7 @@ mod tests {
     use super::*;
     #[test]
     fn it_works() {
-        assert_eq!(fibonacci(3),2);
+        assert_eq!(fibonacci(3),Ok(2));
+        assert_eq!(fibonacci(-1),Err("Cannot be negative".to_string()));
     }
 }
